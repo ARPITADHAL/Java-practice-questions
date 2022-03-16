a9642b3af7b54195b370ee6291ff65271eb21216
arpita dhal <86421844+ARPITADHAL@users.noreply.github.com>
2022-03-16 16:01:18 +0530

    Add files via upload

---
+++ MS_class05.java
@@ -0,0 +1,21 @@
+//CHECKING IF AN INTEGER IS ODD OR EVEN
+
+package com.company;
+
+public class MS_class05 {
+    public static void main(String[] args) {
+        int x = 5;
+        if (((x/2)*2)==x){
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
+        }
+
+        int y = 4;
+        if (((y/2)*2)==y){
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
+        }
+    }
+}
